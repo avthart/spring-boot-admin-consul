d13386f993b18f49021f3350ec0a7ab735914e56
Albert van 't Hart <albert.vanthart@enovation.nl>
2015-07-21 16:04:39 +0200

    updated spring cloud consul to 1.0.0.M1

--- spring-boot-admin/src/main/java/nl/enovation/spring/boot/admin/SpringBootAdminApplication.java
+++ spring-boot-admin/src/main/java/nl/enovation/spring/boot/admin/SpringBootAdminApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.consul.discovery.ConsulDiscoveryClientConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
